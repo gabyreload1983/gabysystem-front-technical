64205cd39619e8e93baf4941de77819016dc1eac
Gabriel Sinapsis <gabyreload@gmail.com>
2023-04-25 14:30:36 -0300

    Styling OrderDetail component

--- src/pages/Orders/OrderDetail/OrderDetail.jsx
+++ src/pages/Orders/OrderDetail/OrderDetail.jsx
@@ -4,11 +4,21 @@ import { useParams } from "react-router-dom";
 import { callApi } from "../utils";
 import ProductsInOrder from "./ProductsInOrder";
 import { UserContext } from "../../../context/userContext";
+import { useNavigate } from "react-router-dom";
+import Table from "react-bootstrap/Table";
 
 export default function OrderDetail() {
   const { user } = useContext(UserContext);
   const { id } = useParams();
   const [order, setOrder] = useState(null);
+
+  console.log(order);
+
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
 
   const getOrders = async () => {
     const response = await callApi(id);
@@ -22,25 +32,87 @@ export default function OrderDetail() {
   return (
     <>
       {order && (
-        <Container>
+        <Container className="mt-3">
+          <Button variant="outline-secondary" onClick={goBack}>
+            &larr; <span>Volver</span>
+          </Button>
           <h2>
             {order.nombre} - {order.nrocompro}
           </h2>
           <Row>
-            <Col xs={6}>
-              <p>ESTADO: {order.estado}</p>
-              <p>DIAGNOSTICO: {order.diag}</p>
-              <p>UBICACION: {order.ubicacion}</p>
-              <p>FECHA INGRESO: {order.ingresado}</p>
-              <p>PRIORIDAD: {order.prioridad}</p>
-              <p>VENDEDOR: {order.operador}</p>
+            <Col xs={12} md={6}>
+              <Table striped bordered hover size="sm">
+                <tbody>
+                  <tr>
+                    <td>ESTADO</td>
+                    <td>{order.estado}</td>
+                  </tr>
+                  <tr>
+                    <td>DIAGNOSTICO</td>
+                    <td>{order.diag}</td>
+                  </tr>
+                  <tr>
+                    <td>UBICACION</td>
+                    <td>{order.ubicacion}</td>
+                  </tr>
+                  <tr>
+                    <td>FECHA INGRESO</td>
+                    <td>{order.ingresado}</td>
+                  </tr>
+                  <tr>
+                    <td>PRIORIDAD</td>
+                    <td>{order.prioridad}</td>
+                  </tr>
+                </tbody>
+              </Table>
             </Col>
-            <Col xs={6}>
-              <p>TELEFONO: {order.telefono}</p>
-              <p>ARTICULO: {order.descart}</p>
-              <p>FALLA: {order.falla}</p>
-              <p>ACCESORIOS: {order.accesorios}</p>
-              <p>TECNICO: {order.tecnico}</p>
+            <Col xs={12} md={6}>
+              <Table striped bordered hover size="sm">
+                <tbody>
+                  <tr>
+                    <td>TELEFONO</td>
+                    <td>{order.telefono}</td>
+                  </tr>
+                  <tr>
+                    <td>ARTICULO</td>
+                    <td>{order.descart}</td>
+                  </tr>
+                  <tr>
+                    <td>ACCESORIOS</td>
+                    <td>{order.accesorios}</td>
+                  </tr>
+                  <tr>
+                    <td>VENDEDOR</td>
+                    <td>{order.operador}</td>
+                  </tr>
+                  <tr>
+                    <td>TECNICO</td>
+                    <td>{order.tecnico}</td>
+                  </tr>
+                </tbody>
+              </Table>
+            </Col>
+            <Col xs={12}>
+              <Table striped bordered hover size="sm">
+                <tbody>
+                  <tr>
+                    <td>
+                      FALLA: <span>{order.falla}</span>
+                    </td>
+                  </tr>
+                </tbody>
+              </Table>
+            </Col>
+            <Col xs={12}>
+              <Table striped bordered hover size="sm">
+                <tbody>
+                  <tr>
+                    <td>
+                      DIAGNOSTICO: <span>{order.diagnostico}</span>
+                    </td>
+                  </tr>
+                </tbody>
+              </Table>
             </Col>
           </Row>
           <Row>
@@ -51,18 +123,24 @@ export default function OrderDetail() {
           </Row>
           <Row>
             <Col>
-              {order.estado === 22 && order.tecnico === user.code_technical && (
-                <ButtonGroup aria-label="Basic example">
-                  <Button variant="primary">Reparado</Button>
-                  <Button variant="danger">Sin Reparacion</Button>
-                  <Button variant="info">Guardar</Button>
-                </ButtonGroup>
-              )}
+              {order.estado === 22 &&
+                order.tecnico === user?.code_technical && (
+                  <ButtonGroup aria-label="Basic example">
+                    <Button variant="primary">Reparado</Button>
+                    <Button variant="danger">Sin Reparacion</Button>
+                    <Button variant="info">Guardar</Button>
+                  </ButtonGroup>
+                )}
             </Col>
             <Col className="text-end">
               {order.estado === 22 && (
                 <ButtonGroup aria-label="Basic example">
                   <Button variant="warning">Liberar</Button>
+                </ButtonGroup>
+              )}
+              {order.estado === 21 && (
+                <ButtonGroup aria-label="Basic example">
+                  <Button variant="outline-success">Tomar</Button>
                 </ButtonGroup>
               )}
             </Col>
