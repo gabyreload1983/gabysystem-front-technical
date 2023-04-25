b6c24ea9e38babd8ee221f0e975f0c993f8c8204
Gabriel Sinapsis <gabyreload@gmail.com>
2023-04-25 08:56:59 -0300

    Update OrderList view render

--- src/pages/Orders/OrderList.jsx
+++ src/pages/Orders/OrderList.jsx
@@ -1,19 +1,55 @@
 import React from "react";
+import { Table } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default function OrderList({ orders }) {
+  const takeOrder = async (nrocompro) => {
+    console.log(nrocompro);
+  };
   return (
     <>
-      {orders &&
-        orders.map((order) => {
-          return (
-            <div key={order.nrocompro}>
-              <p>
-                {order.nrocompro} - {order.codigo} - {order.nombre} -{" "}
-                {order.tecnico}
-              </p>
-            </div>
-          );
-        })}
+      <Table striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>FECHA</th>
+            <th>NRO ORDEN</th>
+            <th>CLIENTE</th>
+            <th>TIER</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders &&
+            orders.map((order) => {
+              return (
+                <tr key={`${order.nrocompro}`}>
+                  <td>{order.ingresado}</td>
+                  <td>
+                    <Link
+                      to={`/orders/${order.nrocompro}`}
+                      className="link-order"
+                    >
+                      {order.nrocompro}
+                    </Link>
+                  </td>
+                  <td>{order.nombre} - </td>
+                  <td>{order.prioridad}</td>
+                  <td>
+                    {order.estado === 21 && (
+                      <Link
+                        className="btn btn-outline-info"
+                        onClick={() => takeOrder(order.nrocompro)}
+                        to={`/orders/${order.nrocompro}`}
+                      >
+                        TOMAR
+                      </Link>
+                    )}
+                  </td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </Table>
     </>
   );
 }
