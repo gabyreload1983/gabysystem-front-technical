80f319ed41abad21d42a414299d3c0b86aa53a4a
Gabriel Sinapsis <gabyreload@gmail.com>
2023-06-07 16:48:50 -0300

    Product component search by description, code or ean Format final price. List1

---
+++ src/components/SearchProduct.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { getFromApi } from "./../utils";
+
+export default function SearchProduct({ onChangeProducts }) {
+  const [input, setInput] = useState([]);
+  const [searchBy, setSearchBy] = useState("description");
+
+  const searchProduct = async () => {
+    const response = await getFromApi(
+      `http://192.168.8.153:3400/api/products/search-by?${searchBy}=${input}`
+    );
+    console.log(response);
+    onChangeProducts(response.payload);
+  };
+
+  const handleSearchChange = (event) => {
+    const input = event.target.value;
+    setInput(input);
+  };
+
+  const handleKeyDown = async (event) => {
+    const keyCode = event.keyCode;
+    if (keyCode === 13) searchProduct();
+    if (keyCode === 115) {
+      document.querySelector("#search").value = "";
+      setSearchBy((prev) => {
+        if (prev === "code") return "ean";
+        if (prev === "ean") return "description";
+        if (prev === "description") return "code";
+      });
+    }
+  };
+
+  const formatSearchBy = (searcBy) => {
+    if (searcBy === "code") return "CODIGO";
+    if (searcBy === "ean") return "AEN";
+    if (searcBy === "description") return "DESCRIPCION";
+  };
+
+  return (
+    <div className="col p-3">
+      <div className="d-flex">
+        <input
+          className="form-control me-2"
+          type="search"
+          placeholder={`${formatSearchBy(searchBy)}`}
+          onChange={handleSearchChange}
+          name="search"
+          onKeyDown={handleKeyDown}
+          id="search"
+        />
+
+        <button className="btn btn-outline-success" onClick={searchProduct}>
+          Buscar
+        </button>
+      </div>
+      <div className="form-text">
+        Presiona F4 para cambiar el tipo de busqueda
+      </div>
+    </div>
+  );
+}
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,10 +1,25 @@
-import React from "react";
-import { Container } from "react-bootstrap";
+import React, { useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import SearchProduct from "../../components/SearchProduct";
+import ProductsList from "./ProductsList";
 
 export default function Products() {
+  const [data, setData] = useState({ products: [], dollar: "" });
+
+  const handleChangeProducts = (data) => {
+    setData(data);
+  };
+
   return (
     <Container>
-      <h1>Products</h1>
+      <Row>
+        <Col xs={12} md={6} lg={4}>
+          <SearchProduct onChangeProducts={handleChangeProducts} />
+        </Col>
+        <Col xs={12}>
+          <ProductsList data={data} />
+        </Col>
+      </Row>
     </Container>
   );
 }
